8c4f591cfbd58b3c3700657a7c1a4697f85101cf
EWS Guest EWS Guest <ewsguest271@siebl-0222-04.ews.illinois.edu>
2018-11-11 11:50:12 -0600

    My ChicTech Website

---
+++ script.js
@@ -0,0 +1,4 @@
+function clickFunction() {
+  //tells the website what to do when a user clicks the button//
+  document.getElementById("display_text").innerHTML = "Welcome to My ChicTech Website"
+}
